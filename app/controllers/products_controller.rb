1ba64fdd8bfb1eadca8000aaa795c5d6f42d6340
Dmitry Khromov <devandart@ya.ru>
2015-09-02 16:33:21 +0300

    check point, fixes for view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,33 @@
 class ProductsController < ApplicationController
+  before_action :init_product, only: [:show, :edit, :update]
+
+  def index
+    @catalog = Catalog.find(params[:catalog_id])
+    @products = @catalog.products
+  end
+
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update_attributes(product_params)
+      redirect_to catalog_products_path(catalog_id: @product.catalog.id), notes: 'Success'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def init_product
     @product = Product.find(params[:id])
   end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :catalog_id)
+  end
+
 end
