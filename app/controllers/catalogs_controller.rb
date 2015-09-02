31192139e06f99b88c8f717cf4f66420bea262ff
Dmitry Khromov <devandart@ya.ru>
2015-09-02 16:18:27 +0300

    some views for category and products

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -1,6 +1,6 @@
 class CatalogsController < ApplicationController
 
-  before_action :init_catalog, only: [:show, :edit]
+  before_action :init_catalog, only: [:show, :edit, :destroy, :update]
 
   def index
     @catalogs = Catalog.all
@@ -14,7 +14,28 @@ class CatalogsController < ApplicationController
 
   def destroy
     @catalog.destroy
-    redirect_to catalog_path, notice: "Success"
+    redirect_to catalogs_path, notice: "Success"
+  end
+
+  def update
+    if @catalog.update_attributes(catalog_params)
+      redirect_to catalogs_path, notice: 'Success'
+    else
+      render :edit
+    end
+  end
+
+  def new
+    @catalog = Catalog.new
+  end
+
+  def create
+    @catalog = Catalog.new(catalog_params)
+    if @catalog.save
+      redirect_to catalogs_path, notice: 'Success'
+    else
+      render :new
+    end
   end
 
 
@@ -22,5 +43,9 @@ class CatalogsController < ApplicationController
 
   def init_catalog
     @catalog = Catalog.find(params[:id])
+  end
+
+  def catalog_params
+    params.require(:catalog).permit(:name, :description)
   end
 end
